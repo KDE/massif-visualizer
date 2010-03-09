f950765c1fec32d452c11fae71190fd1e8370d26
Milian Wolff <mail@milianw.de>
2010-03-09 12:16:48 +0100

    initial import

---
+++ model/parser.h
@@ -0,0 +1,30 @@
+/*
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef MASSIF_PARSER_H
+#define MASSIF_PARSER_H
+
+namespace Massif {
+
+class Parser
+{
+public:
+    
+};
+
+}
+
+#endif // MASSIF_PARSER_H

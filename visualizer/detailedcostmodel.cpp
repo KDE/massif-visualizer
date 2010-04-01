4d2a378a3fbfa7af71b122e54e2fc8c384b98a51
Milian Wolff <mail@milianw.de>
2010-04-01 13:53:37 +0200

    always return a proper tooltip, esp. for cases where we have a peak that drops to zero / below threshold, the last segment used to not show any tooltip

--- visualizer/detailedcostmodel.cpp
+++ visualizer/detailedcostmodel.cpp
@@ -199,23 +199,15 @@ QVariant DetailedCostModel::data(const QModelIndex& index, int role) const
                 break;
             }
         }
-        if (!node) {
-            if (role == Qt::ToolTipRole) {
-                return QVariant();
-            } else {
-                return 0;
-            }
+        if (role == Qt::ToolTipRole) {
+            return i18n("cost of %1, i.e. %2% of snapshot #%3\n%4",
+                        prettyCost(node ? node->cost() : 0),
+                        // yeah nice how I round to two decimals, right? :D
+                        double(int(double(node ? node->cost() : 0)/snapshot->memHeap()*10000))/100,
+                        snapshot->number(),
+                        needle);
         } else {
-            if (role == Qt::ToolTipRole) {
-                return i18n("cost of %1, i.e. %2% of snapshot #%3\n%4",
-                            prettyCost(node->cost()),
-                            // yeah nice how I round to two decimals, right? :D
-                            double(int(double(node->cost())/snapshot->memHeap()*10000))/100,
-                            snapshot->number(),
-                            node->label());
-            } else {
-                return double(node->cost());
-            }
+            return double(node ? node->cost() : 0);
         }
     }
 }

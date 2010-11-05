5785182b9827522f24da712cd63694f1396ab24b
Milian Wolff <mail@milianw.de>
2010-11-05 04:43:16 +0100

    add Messages.sh

---
+++ Messages.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+subdirs="app kdchart massifdata visualizer"
+rcfiles="`find $subdirs -name \*.rc`"
+uifiles="`find $subdirs -name \*.ui`"
+kcfgfiles="`find $subdirs -name \*.kcfg`"
+if [[ "$rcfiles" != "" ]] ; then
+    $EXTRACTRC $rcfiles >> rc.cpp || exit 11
+fi
+if [[ "$uifiles" != "" ]] ; then
+    $EXTRACTRC $uifiles >> rc.cpp || exit 12
+fi
+if [[ "$kcfgfiles" != "" ]] ; then
+    $EXTRACTRC $kcfgfiles >> rc.cpp || exit 13
+fi
+$XGETTEXT -kaliasLocal `find $subdirs -name \*.cc -o -name \*.cpp -o -name \*.h` rc.cpp -o $podir/massifvisualizer.pot
+rm -f rc.cpp

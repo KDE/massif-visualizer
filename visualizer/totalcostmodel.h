d83e7d82b8538a05e0bc8d2f46dd97324d05e766
Milian Wolff <mail@milianw.de>
2010-11-13 17:56:06 +0100

    add myself as copyright holder

--- visualizer/totalcostmodel.h
+++ visualizer/totalcostmodel.h
@@ -1,4 +1,8 @@
 /*
+   This file is part of Massif Visualizer
+
+   Copyright 2010 Milian Wolff <mail@milianw.de>
+
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
    License version 2 as published by the Free Software Foundation.
